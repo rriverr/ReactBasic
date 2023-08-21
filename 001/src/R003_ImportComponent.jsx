88fd15887239ef05449e4d47ce3f7eb6861d1564
yuna <playamongthestars000@gmail.com>
2023-08-21 15:42:47 +0900

    [feat] 008 finished

---
+++ 001/src/R003_ImportComponent.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+class R003_ImportComponent extends React.Component {
+  render() {
+    return <h2>[THIS IS IMPORTED COMPONENT]</h2>;
+  }
+}
+
+export default R003_ImportComponent;
