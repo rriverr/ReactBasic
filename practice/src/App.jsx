10c626d4a558035032a55a7d15646af5eda562dd
yuna <playamongthestars000@gmail.com>
2023-08-25 11:21:03 +0900

    useState 실습

--- practice/src/App.jsx
+++ practice/src/App.jsx
@@ -1,11 +1,18 @@
-import React from "react";
+import React from 'react';
 
-import "./styles.css";
+import './styles.css';
+
 // don't change the Component name "App"
+// important: In this code editor, use React.useState() instead of just useState()
 export default function App() {
-  const onClickButton = () => {
-    console.log("Stored!");
-  }
-
-  return <button onClick={onClickButton}>Bookmark</button>;
+    const [price, setPrice] = React.useState(100);
+    const sale = () => {
+      setPrice(75);
+    }
+    return (
+        <div>
+            <p>${price}</p>
+            <button onClick={sale}>Apply Discount</button>
+        </div>
+    );
 }
